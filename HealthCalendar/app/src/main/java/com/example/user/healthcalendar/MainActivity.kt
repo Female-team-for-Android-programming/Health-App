8ccb8b71b118124e433f6b8a4d3679e85ccac9bd
galyaminavas <galyaminavas@gmail.com>
2018-03-22 23:20:04 +0300

    Fragments for menu items added

--- HealthCalendar/app/src/main/java/com/example/user/healthcalendar/MainActivity.kt
+++ HealthCalendar/app/src/main/java/com/example/user/healthcalendar/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.user.healthcalendar
 
+import android.app.FragmentTransaction
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -8,10 +9,22 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.user.healthcalendar.Fragments.*
+import com.example.user.healthcalendar.R.id.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
+    val fcalendar = FragmentCalendar()
+    val fdoctors = FragmentDoctors()
+    val fmeds = FragmentMeds()
+    val fexam = FragmentExam()
+    val fjournal = FragmentJournal()
+    val fmanage = FragmentManage()
+    val fhome = FragmentHome()
+    //
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +42,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        //var fdoctors : FragmentDoctors = FragmentDoctors()
+
     }
 
     override fun onBackPressed() {
@@ -57,29 +73,33 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
+
+        val ftrans = supportFragmentManager.beginTransaction()
+
         when (item.itemId) {
             R.id.nav_home -> {
-                // Handle the camera action
+                ftrans.replace(R.id.container, fhome)
             }
             R.id.nav_calendar -> {
-
+                ftrans.replace(R.id.container, fcalendar)
             }
             R.id.nav_doctors -> {
-
+                ftrans.replace(R.id.container, fdoctors)
             }
             R.id.nav_meds -> {
-
+                ftrans.replace(R.id.container, fmeds)
             }
             R.id.nav_exam -> {
-
+                ftrans.replace(R.id.container, fexam)
             }
             R.id.nav_journal -> {
-
+                ftrans.replace(R.id.container, fjournal)
             }
             R.id.nav_manage -> {
-
+                ftrans.replace(R.id.container, fmanage)
             }
         }
+        ftrans.commit()
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
